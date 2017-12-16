4f0f26b71725553a763835521467682c12166ed9
Ari Asulin <ari.asulin@gmail.com>
2017-12-15 22:40:59 -0700

    refactor

--- site/script/git.php
+++ site/script/git.php
@@ -6,19 +6,6 @@
  * Time: 4:07 PM
  */
 
-
-$params = file_get_contents('php://input');
-if($params && $params[0] === '{') {
-    $params = json_decode($params, true);
-} else {
-    $params = $_REQUEST;
-}
-
-//$path = $params['path'];
-//$data = $params['data'];
-
-$root = dirname(dirname(dirname(dirname(__DIR__))));
-$realpath = realpath($root . '/' . $path); //  . rand(1111, 9999) . '.save.png'
-
-
-system("/usr/bin/git pull");
+header("Content-Type: text/plain");
+echo "Executing Git Pull...\n";
+echo exec("git pull", $result);
