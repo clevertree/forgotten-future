73cfd2ab1f51ca842013a5fda7f00fb18436c59b
Ari Asulin <ari.asulin@gmail.com>
2018-01-20 15:59:36 -0700

    paypal

--- site/payment/paypal/api.php
+++ site/payment/paypal/api.php
@@ -3,6 +3,11 @@
 require('PaypalIPN.php');
 
 use PaypalIPN;
+use Site\DB\Table\PaymentRow;
+use Site\DB\Table\UserRow;
+
+chdir('../../../');
+spl_autoload_register();
 
 $ipn = new PaypalIPN();
 
@@ -19,14 +24,29 @@ try {
          * https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNandPDTVariables/
          */
     }
+    $Row = storeVerifiedEntry($_POST);
     // Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
     header("HTTP/1.1 200 OK");
     header("Content-Type: text/plain");
-    print_r($_POST);
+    error_log("PAYPAL ROW " . print_r($Row, true));
+
 } catch (\Exception $e) {
     header("HTTP/1.1 400 " . $e->getMessage());
     header("Content-Type: text/plain");
     echo $e;
-    print_r($_POST);
+    error_log("PAYPAL RESPONSE FAILED: " . $e);
 }
 
+
+
+function storeVerifiedEntry($post, UserRow $UserRow=null) {
+    return PaymentRow::createNewPaymentEntry(
+        $post['payment_status'],
+        $post['payment_type'],
+        $post['mc_gross'],
+        $post['payer_email'],
+        $post['first_name'] . ' ' . $post['last_name'],
+        $UserRow,
+        $post
+    );
+}
