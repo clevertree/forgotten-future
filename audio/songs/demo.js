6c356b2995709031674d0e3004d9242b9bac77e3
Ari Asulin <ari.asulin@gmail.com>
2018-05-31 19:01:14 -0700

    remove duplicates

--- audio/songs/demo.js
+++ audio/songs/demo.js
@@ -1,9 +1,32 @@
-// one context per document
-var context = new (window.AudioContext || window.webkitAudioContext)();
-var osc = context.createOscillator(); // instantiate an oscillator
-osc.type = 'sine'; // this is the default - also square, sawtooth, triangle
-osc.frequency.value = 555; // Hz
-osc.connect(context.destination); // connect it to the destination
-osc.start(); // start the oscillator
-osc.stop(context.currentTime + 2); // stop 2 seconds after the current time
-console.log('playing')
+
+
+(function() {
+    document.addEventListener('song:load', onLoad);
+
+    function onLoad(e) {
+        var manager = new (SongManager || e.detail.SongManager)("Song Title");
+
+        manager.addNotes();
+        manager.setOnPlay(onPlay);
+        
+    }
+
+    function onPlay(e) {
+        // one context per document
+        var context = e.detail.context || new (window.AudioContext || window.webkitAudioContext)();
+        var osc = context.createOscillator(); // instantiate an oscillator
+        osc.type = 'sine'; // this is the default - also square, sawtooth, triangle
+        osc.frequency.value = 555; // Hz
+        osc.connect(context.destination); // connect it to the destination
+        osc.start(); // start the oscillator
+        osc.stop(context.currentTime + 2); // stop 2 seconds after the current time
+        console.log('playing');
+
+    }
+
+    
+    function SongManager(title) {
+        
+    }
+    
+})();
