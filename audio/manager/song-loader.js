fe6c6357aa6d5b240041c4bdd155df3bc2b7f679
Ari Asulin <ari.asulin@gmail.com>
2018-06-07 09:03:38 -0700

    song loader updates

--- audio/manager/song-loader.js
+++ audio/manager/song-loader.js
@@ -26,6 +26,8 @@
         // this.groups = [];
     }
 
+    // Playback Commands
+
     SongLoader.prototype.play = function(context, onPlaybackStarted) {
         this.context = (context || new (window.AudioContext || window.webkitAudioContext)());
         // if(this.notes) {
@@ -43,32 +45,6 @@
         }.bind(this));
         // }
     };
-
-    SongLoader.prototype.getNoteFrequency = function (note) {
-        if(Number(note) === note && note % 1 !== 0)
-            return note;
-        var notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
-            octave,
-            keyNumber;
-
-        if (note.length === 3) {
-            octave = note.charAt(2);
-        } else {
-            octave = note.charAt(1);
-        }
-
-        keyNumber = notes.indexOf(note.slice(0, -1));
-
-        if (keyNumber < 3) {
-            keyNumber = keyNumber + 12 + ((octave - 1) * 12) + 1;
-        } else {
-            keyNumber = keyNumber + ((octave - 1) * 12) + 1;
-        }
-
-        // Return frequency of note
-        return 440 * Math.pow(2, (keyNumber- 49) / 12);
-    };
-
 
     SongLoader.prototype.processPlayback = function() {
         this.currentPosition = 0;
@@ -98,6 +74,114 @@
                 detail: this
             }));
         }
+    };
+
+    // Interface Commands
+
+    SongLoader.prototype.getInstrument = function(path) {
+        if(!window.instruments)
+            throw new Error("window.instruments is not loaded");
+
+        var pathList = path.split('.');
+        var pathTarget = window.instruments;
+
+        if(this.aliases[pathList[0]])
+            pathList[0] = this.aliases[pathList[0]];
+
+        for (var i = 0; i < pathList.length; i++) {
+            if (pathTarget[pathList[i]]) {
+                pathTarget = pathTarget[pathList[i]];
+            } else {
+                pathTarget = null;
+                break;
+            }
+        }
+        if (pathTarget && typeof pathTarget === 'object')
+            pathTarget = pathTarget.default;
+        if (!pathTarget)
+            throw new Error("Instrument not found: " + pathList.join('.') + ' [alias:' + path + ']');
+        return pathTarget;
+    };
+
+    SongLoader.prototype.getNoteFrequency = function (note) {
+        if(Number(note) === note && note % 1 !== 0)
+            return note;
+        var notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
+            octave,
+            keyNumber;
+
+        if (note.length === 3) {
+            octave = note.charAt(2);
+        } else {
+            octave = note.charAt(1);
+        }
+
+        keyNumber = notes.indexOf(note.slice(0, -1));
+
+        if (keyNumber < 3) {
+            keyNumber = keyNumber + 12 + ((octave - 1) * 12) + 1;
+        } else {
+            keyNumber = keyNumber + ((octave - 1) * 12) + 1;
+        }
+
+        // Return frequency of note
+        return 440 * Math.pow(2, (keyNumber- 49) / 12);
+    };
+
+    // Loading Methods
+
+    SongLoader.prototype.loadFile = function(onLoaded) {
+        var filePath = this.filePath;
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === xhr.DONE) {
+                if (xhr.status === 200 && xhr.response) {
+                    this.processNoteList(xhr.response, onLoaded);
+
+                } else {
+                    console.log("Failed to download:" + xhr.status + " " + xhr.statusText);
+                }
+            }
+        }.bind(this);
+        // Open the request for the provided url
+        xhr.open("GET", filePath, true);
+        // Set the responseType to 'arraybuffer' for ArrayBuffer response
+        xhr.responseType = "arraybuffer";
+        xhr.send();
+    };
+
+    SongLoader.prototype.processNoteList = function(arrayBuffer, onLoaded) {
+        var charBuffer = new Uint8Array(arrayBuffer, 0);
+        var byteLength = charBuffer.byteLength;
+
+        // Iterate through each character in our Array
+        this.groups = {default:[]};
+
+        var lastCharBuffer = '';
+        var commandList = [];
+        for (var i = 0; i < byteLength; i++) {
+            // Get the character for the current iteration
+            var char = String.fromCharCode(charBuffer[i]);
+            lastCharBuffer += char;
+            switch(char) {
+                case ')':
+                    lastCharBuffer = lastCharBuffer.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1').trim();
+                    var match = /([a-z]+)\(([^)]+)\)/.exec(lastCharBuffer);
+                    if(match) {
+                        var commandName = match[1];
+                        var commandArgs = match[2].split(/\s*,\s*/);
+                        commandArgs.unshift(commandName);
+                        lastCharBuffer = '';
+                        commandList.push(commandArgs);
+                        // console.log('Processed Note List: ', lastCharBuffer, commandArgs);
+
+                    }
+                    break;
+            }
+        }
+
+        console.log('Loaded Command List: ', commandList);
+        this.loadCommandList(commandList, onLoaded);
     };
 
     SongLoader.prototype.loadCommandList = function(commandList, onLoaded) {
@@ -192,99 +276,8 @@
 
     };
 
-    SongLoader.prototype.getInstrument = function(path) {
-        if(!window.instruments)
-            throw new Error("window.instruments is not loaded");
 
-        var pathList = path.split('.');
-        var pathTarget = window.instruments;
-
-        if(this.aliases[pathList[0]])
-            pathList[0] = this.aliases[pathList[0]];
-
-        for (var i = 0; i < pathList.length; i++) {
-            if (pathTarget[pathList[i]]) {
-                pathTarget = pathTarget[pathList[i]];
-            } else {
-                pathTarget = null;
-                break;
-            }
-        }
-        if (pathTarget && typeof pathTarget === 'object')
-            pathTarget = pathTarget.default;
-        if (!pathTarget)
-            throw new Error("Instrument not found: " + pathList.join('.') + ' [alias:' + path + ']');
-        return pathTarget;
-    };
-
-    // File Methods
-
-    SongLoader.prototype.loadFile = function(onLoaded) {
-        var filePath = this.filePath;
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === xhr.DONE) {
-                if (xhr.status === 200 && xhr.response) {
-                    this.processNoteList(xhr.response, onLoaded);
-
-                } else {
-                    console.log("Failed to download:" + xhr.status + " " + xhr.statusText);
-                }
-            }
-        }.bind(this);
-        // Open the request for the provided url
-        xhr.open("GET", filePath, true);
-        // Set the responseType to 'arraybuffer' for ArrayBuffer response
-        xhr.responseType = "arraybuffer";
-        xhr.send();
-    };
-
-    SongLoader.prototype.processNoteList = function(arrayBuffer, onLoaded) {
-        var charBuffer = new Uint8Array(arrayBuffer, 0);
-        var byteLength = charBuffer.byteLength;
-
-        // Iterate through each character in our Array
-        this.groups = {default:[]};
-
-        var lastCharBuffer = '';
-        var commandList = [];
-        for (var i = 0; i < byteLength; i++) {
-            // Get the character for the current iteration
-            var char = String.fromCharCode(charBuffer[i]);
-            lastCharBuffer += char;
-            switch(char) {
-                case ')':
-                    lastCharBuffer = lastCharBuffer.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1').trim();
-                    var match = /([a-z]+)\(([^)]+)\)/.exec(lastCharBuffer);
-                    if(match) {
-                        var commandName = match[1];
-                        var commandArgs = match[2].split(/\s*,\s*/);
-                        commandArgs.unshift(commandName);
-                        lastCharBuffer = '';
-                        commandList.push(commandArgs);
-                        // console.log('Processed Note List: ', lastCharBuffer, commandArgs);
-
-                    }
-                    break;
-            }
-        }
-
-        console.log('Loaded Command List: ', commandList);
-        this.loadCommandList(commandList, onLoaded);
-    };
-
-
-
-    function parseValue(value) {
-        value = value.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1').trim();
-        if(!isNaN(parseFloat(value)) && isFinite(value))
-            value = parseFloat(value);
-
-        return value;
-    }
-
-
-    // Instrument Command
+    // Instrument Commands
 
     /**
      * Pause Instrument
